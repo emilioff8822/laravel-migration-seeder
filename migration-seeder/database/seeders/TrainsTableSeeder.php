34671443aa9a08ea9be78b1c05d43812c5a93704
emilioff8822 <emilioff@hotmail.it>
2023-06-08 17:13:01 +0200

    commit-seeder-fakers2

--- migration-seeder/database/seeders/TrainsTableSeeder.php
+++ migration-seeder/database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -15,20 +16,21 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $companies = ['Company A', 'Company B', 'Company C'];
+        $stations = ['Station 1', 'Station 2', 'Station 3', 'Station 4', 'Station 5'];
+
         for ($i=0; $i <10 ; $i++) {
             $new_Train = new Train();
-            $new_Train->company = $faker->company;
-            $new_Train->departure_station = $faker->city;
-            $new_Train->arrival_station = $faker->city;
+            $new_Train->company = $faker->randomElement($companies);
+            $new_Train->departure_station = $faker->randomElement($stations);
+            $new_Train->arrival_station = $faker->randomElement($stations);
             $new_Train->departure_time = $faker->dateTimeBetween('-1 years', 'now');
-            $new_Train->arrival_time = $faker->dateTimeBetween('-1 years', 'now');
-            $new_Train->train_code = $faker->bothify('Train-##??');
+            $new_Train->arrival_time = $faker->dateTimeBetween($new_Train->departure_time, '+5 hours');
+            $new_Train->train_code = Str::upper(Str::random(3)) . '-' . $faker->randomNumber(3);
             $new_Train->number_of_carriages = $faker->numberBetween(1, 10);
             $new_Train->is_on_time = $faker->boolean;
             $new_Train->is_cancelled = $faker->boolean;
-            $new_Train->save();
-            //var_dump($new_Train);
-
+            var_dump($new_Train);
         }
     }
 }
