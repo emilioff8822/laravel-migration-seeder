23a91d0bfea4baeffd3387e059af36764a64d2c9
emilioff8822 <emilioff@hotmail.it>
2023-06-08 16:46:54 +0200

    commit-bonus-fakers

---
+++ migration-seeder/database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i <10 ; $i++) {
+            $new_Train = new Train();
+            $new_Train->company = $faker->company;
+            $new_Train->departure_station = $faker->city;
+            $new_Train->arrival_station = $faker->city;
+            $new_Train->departure_time = $faker->dateTimeBetween('-1 years', 'now');
+            $new_Train->arrival_time = $faker->dateTimeBetween('-1 years', 'now');
+            $new_Train->train_code = $faker->bothify('Train-##??');
+            $new_Train->number_of_carriages = $faker->numberBetween(1, 10);
+            $new_Train->is_on_time = $faker->boolean;
+            $new_Train->is_cancelled = $faker->boolean;
+            $new_Train->save();
+        }
+    }
+}
